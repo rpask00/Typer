4483113f47fac7ca8e968267b25f07fd5b573df4
rpask00 <rp.261000@gmail.com>
2020-09-21 11:27:02 +0200

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextFieldComponent } from './dashboard/text-field/text-field.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StatsComponent } from './dashboard/stats/stats.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextFieldComponent,
+    DashboardComponent,
+    StatsComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,

90a4e161d400c70e697bdaf6b01ff0f77b1ba9b4
Rafał Paśko <33097890+rpask00@users.noreply.github.com>
2021-01-20 17:27:09 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,21 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDdbSRyImDUGFiH8PtkszoCSm_b_v8qJsA",
-    authDomain: "typer-f4382.firebaseapp.com",
-    databaseURL: "https://typer-f4382.firebaseio.com",
-    projectId: "typer-f4382",
-    storageBucket: "typer-f4382.appspot.com",
-    messagingSenderId: "895619841289",
-    appId: "1:895619841289:web:252bae5fed549be574a7d8"
+
   }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.

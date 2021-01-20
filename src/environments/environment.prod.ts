52abcfa91a770f10c7c0bd112b675068721e0616
Rafał Paśko <33097890+rpask00@users.noreply.github.com>
2021-01-20 17:26:25 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDdbSRyImDUGFiH8PtkszoCSm_b_v8qJsA",
-    authDomain: "typer-f4382.firebaseapp.com",
-    databaseURL: "https://typer-f4382.firebaseio.com",
-    projectId: "typer-f4382",
-    storageBucket: "typer-f4382.appspot.com",
-    messagingSenderId: "895619841289",
-    appId: "1:895619841289:web:252bae5fed549be574a7d8"
+    
   }
 };
